c917195e5f85fd223f747b4b367127c36cbb1c30
asugurali <asugurali@greet-go.com>
2023-05-31 19:25:13 +0600

    feat(): started impl of welcome-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mediQR-web';
+
+  constructor(
+    private readonly router: Router,
+  ) {
+  }
+
+  ngOnInit() {
+    console.log('oiUnx1WGl5 :: app-root : started')
+    this.router.navigate(['/welcome-page']).then();
+    console.log('mg2RCmZ38B :: app-root : finished')
+  }
 }
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePageComponent implements OnInit {
 
+  isOpenPopover: boolean = false;
+  reasonDisplayValue: string = 'Choose';
+
+  reasonsMap: Map<string, string> = new Map<string, string>();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.reasonsMap.set('ZwyX8o5q', 'Doctor\'s appointment');
+    this.reasonsMap.set('C5040S9R', 'Analyzes');
+    this.reasonsMap.set('eFlLG84G', 'Ultrasound');
+    this.reasonsMap.set('TnXcfF64', 'X-Ray');
+    this.reasonsMap.set('4Ml0CyeY', 'Certificate of illness');
+    this.reasonsMap.set('giDSXMz2', 'Sick leave');
+
+    this.reasonsMap.forEach((map) => {
+      console.log('O7PGJuKM :: map : ', map);
+    })
+  }
+
+  openOrClosePopover() {
+    console.log('rxZ3EZ1BSe :: this.isOpenPopover : before', this.isOpenPopover);
+    this.isOpenPopover = !this.isOpenPopover;
+    console.log('rxZ3EZ1BSe :: this.isOpenPopover : after', this.isOpenPopover);
   }
 
 }
