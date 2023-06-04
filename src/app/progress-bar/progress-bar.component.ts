f02ea2bb45c6557b0b87b89eefa9b84b7b351fb0
Arman <aaj.aro@mail.ru>
2023-06-04 13:19:40 +0600

    fix(): refactor progress-bar component

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -5,7 +5,7 @@ import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
   templateUrl: './progress-bar.component.html',
   styleUrls: ['./progress-bar.component.scss']
 })
-export class ProgressBarComponent {
+export class ProgressBarComponent implements OnInit{
   @Input() queueNumber: number;
   @Input() numberOfPeople: number;
 
@@ -13,24 +13,46 @@ export class ProgressBarComponent {
   @ViewChild('progContainer', { static: true }) progContainer: ElementRef;
 
   progressValue = 0;
-  progressEndValue = 100;
-  speed = 50;
+  progressEndValue = 0;
+  speed = 0;
+  tempValue = 0;
+  currentPercent = 0;
+
+  ngOnInit(): void {
+    this.progressEndValue = this.getWaitingTime() * 60 * 1000; // in milliseconds
+    this.progressValue = 0;
+    this.tempValue = this.getWaitingTime() * 0.6;
+    this.speed = 100;
+  }
 
   ngAfterViewInit() {
     let progressBar = this.progBar.nativeElement;
     let progressContainer = this.progContainer.nativeElement;
 
+    this.currentPercent = 0;
+
     let progress = setInterval(() => {
-      this.progressValue++;
-      console.log("fnxEQv67 :: this.progressValue : ", this.progressValue)
-      progressContainer.textContent = this.progressValue + '%';
-      progressBar.style.background = 'conic-gradient(' +
-        '#4d5bf9' + ' ' + this.progressValue * 3.6 + 'deg,' +
-        '#cadcff' + ' ' + this.progressValue * 3.6 + 'deg' +
-        ')';
-      if (this.progressValue === this.progressEndValue) {
+      this.progressValue = Number.parseFloat((this.progressValue + 0.1).toFixed(1));
+
+      if ((this.progressValue / this.tempValue).toFixed(0) === (this.currentPercent + 1).toString()) {
+
+        this.currentPercent += 1;
+
+        progressContainer.textContent = this.currentPercent.toString() + '%';
+        progressBar.style.background = 'conic-gradient(' +
+          '#4d5bf9' + ' ' + this.currentPercent * 3.6 + 'deg,' +
+          '#cadcff' + ' ' + this.currentPercent * 3.6 + 'deg' +
+          ')';
+      }
+
+      if (this.progressValue === this.progressEndValue / 1000) {
+        console.log('Cgg1ThdE :: this.progressValue === this.progressEndValue / 1000 : ', this.progressValue === this.progressEndValue / 1000)
         clearInterval(progress)
       }
     }, this.speed)
   };
+
+  getWaitingTime(): number {
+    return this.numberOfPeople * 5; // for each person gives 5 minutes
+  }
 }
